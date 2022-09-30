32a92236e6a44781c636f5d2b3c1c0825d086e22
fulin leow <nonamime@gmail.com>
2022-09-30 14:39:11 +0800

    Add http module and setup dev api proxy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Test1Module } from './test1/test1.module';
+import { Test3221Component } from './test1/test3221/test3221.component';
+import { UserService } from './service/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    UserService,
+  ],
+  bootstrap: [
+    AppComponent,
+    Test3221Component
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
