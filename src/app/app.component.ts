ff504022d01f7bd38798f7196c19df6b9ffb3a2f
fulin leow <nonamime@gmail.com>
2022-10-03 10:17:01 +0800

    Add sidebar with screen width detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { BooleanInput } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gt-salary-frontend';
+export class AppComponent implements OnInit {
+
+  drawerOpened: BooleanInput = true;
+
+  constructor(private responsive: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.responsive.observe([
+      Breakpoints.Large,
+      Breakpoints.Medium,
+      Breakpoints.Small])
+      .subscribe(
+        result => {
+          console.log(this.drawerOpened, result.breakpoints)
+          if (result.breakpoints["(min-width: 960px) and (max-width: 1279.98px)"]) {
+            this.drawerOpened = true;
+          }
+          if (result.breakpoints["(min-width: 1280px) and (max-width: 1919.98px)"]) {
+            this.drawerOpened = true;
+          }
+          if (result.breakpoints["(min-width: 600px) and (max-width: 959.98px)"]) {
+            this.drawerOpened = false;
+          }
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { Test1Module } from './test1/test1.module';
-import { Test3221Component } from './test1/test3221/test3221.component';
-import { UserService } from './service/user.service';
+import { UserService } from '@service/user.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -34,7 +32,6 @@ import { MatDividerModule } from '@angular/material/divider'
   ],
   bootstrap: [
     AppComponent,
-    Test3221Component
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
